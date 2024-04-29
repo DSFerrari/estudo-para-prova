caae6e0e830da8c6c3306ea480b6652f5838054e
Diego <diegosferrari@outlook.com>
2024-04-29 16:09:47 -0300

    componentes adicionados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastrarAlunosComponent } from './cadastrar-alunos/cadastrar-alunos.component';
+import { CadastrarProdutosComponent } from './cadastrar-produtos/cadastrar-produtos.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastrarAlunosComponent,
+    CadastrarProdutosComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cadastrar-alunos/cadastrar-alunos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-alunos',
+  templateUrl: './cadastrar-alunos.component.html',
+  styleUrl: './cadastrar-alunos.component.css'
+})
+export class CadastrarAlunosComponent {
+
+}
